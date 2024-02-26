14c908c2e4e645ea836f7024fdfa2f8559405af3
username <wecp-dummy@gmail.com>
2024-02-26 18:07:18 +0000

    Task User Done Security In Progress

--- server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/controller/RegisterAndLoginController.java
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/controller/RegisterAndLoginController.java
@@ -5,31 +5,59 @@ import com.wecp.medicalequipmentandtrackingsystem.dto.LoginRequest;
 import com.wecp.medicalequipmentandtrackingsystem.dto.LoginResponse;
 import com.wecp.medicalequipmentandtrackingsystem.entitiy.User;
 import com.wecp.medicalequipmentandtrackingsystem.jwt.JwtUtil;
+import com.wecp.medicalequipmentandtrackingsystem.repository.UserRepository;
 import com.wecp.medicalequipmentandtrackingsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+// import org.springframework.security.core.Authentication;
+// import org.springframework.security.core.AuthenticationException;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-
+@RestController
+@RequestMapping("/api/user")
 public class RegisterAndLoginController {
 
+    @Autowired
+    private UserService userService;
 
-    @PostMapping("/api/user/register")
+    // @Autowired
+    // AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+
+    @PostMapping("/register")
     public ResponseEntity<User> registerUser(@RequestBody User user) {
+
+        return new ResponseEntity<>(userService.registerUser(user), HttpStatus.CREATED);
+
         // register user and return the registered user with status code 201 created
     }
 
-    @PostMapping("/api/user/login")
-    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest) {
-        // login user and return the login response with status code 200 ok
-        // if authentication fails, return status code 401 unauthorized
-    }
+    // @PostMapping("/login")
+    // public ResponseEntity<String> loginUser(@RequestBody LoginRequest loginRequest) {
+        
+    //     Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+    //     if (authentication.isAuthenticated()) {
+    //         return ResponseEntity.ok(jwtUtil.generateToken(loginRequest.getUsername()));
+    //     } else {
+    //         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    //     }
+        
+
+
+    
+    //     // login user and return the login response with status code 200 ok
+    //     // if authentication fails, return status code 401 unauthorized
+    // }
 }
