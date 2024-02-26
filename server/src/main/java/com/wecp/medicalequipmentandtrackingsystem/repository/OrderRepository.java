4d901537951e45de93e3ac4a9743bb4da8f0685b
username <wecp-dummy@gmail.com>
2024-02-26 17:29:55 +0000

    supplier controller done

--- server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/repository/OrderRepository.java
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/repository/OrderRepository.java
@@ -1,14 +1,10 @@
 package com.wecp.medicalequipmentandtrackingsystem.repository;
-
-
-import com.wecp.medicalequipmentandtrackingsystem.entitiy.Order;
+ 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-
-public interface OrderRepository  {
-
-    // extend jpa repository and add method if needed
+ 
+import com.wecp.medicalequipmentandtrackingsystem.entitiy.OrderData;
+ 
+@Repository
+public interface OrderRepository extends JpaRepository<OrderData,Long>{
 }
