243cdf8fdc7a34eae894a1c3cc4355a75ac6f124
username <wecp-dummy@gmail.com>
2024-02-27 06:37:01 +0000

    Equipment Backend Merge

--- server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/repository/EquipmentRepository.java
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/repository/EquipmentRepository.java
@@ -1,13 +1,15 @@
 package com.wecp.medicalequipmentandtrackingsystem.repository;
-
-
+ 
+ 
 import com.wecp.medicalequipmentandtrackingsystem.entitiy.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 import java.util.List;
-
-
-public interface EquipmentRepository {
+ 
+@Repository
+public interface EquipmentRepository extends JpaRepository<Equipment,Long>{
     // extent jpa repository and add custom methods if needed
+    public List<Equipment> findByHospitalId(Long HospitalId);
+ 
 }
--- server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/service/EquipmentService.java
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/service/EquipmentService.java
@@ -1,29 +1,43 @@
 package com.wecp.medicalequipmentandtrackingsystem.service;
-
-
+ 
+ 
 import com.wecp.medicalequipmentandtrackingsystem.entitiy.Equipment;
 import com.wecp.medicalequipmentandtrackingsystem.entitiy.Hospital;
 import com.wecp.medicalequipmentandtrackingsystem.repository.EquipmentRepository;
 import com.wecp.medicalequipmentandtrackingsystem.repository.HospitalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+ 
 import javax.persistence.EntityNotFoundException;
+ 
+import java.sql.SQLException;
 import java.util.List;
-
-
+ 
+@Service
 public class EquipmentService {
-
-
+    @Autowired
+    private EquipmentRepository equipmentRepository;
+ 
+    @Autowired
+    private HospitalRepository hospitalRepository;
+ 
+ 
     public Equipment addEquipment(Long hospitalId, Equipment equipment) {
+ 
         // check if hospital exists
-
+        Hospital hospital=hospitalRepository.findById(hospitalId).orElseThrow(()->new EntityNotFoundException("Hospital with "+hospitalId+" doesnot exists."));
+        equipment.setHospital(hospital);
+        return equipmentRepository.save(equipment);
+ 
         // add equipment to hospital
     }
-
-    public List<Equipment> getAllEquipmentOfHospital(Long hospitalId) {
+ 
+    public List<Equipment> getAllEquipmentOfHospital(Long hospitalId) throws SQLException{
         // return all equipments of hospital
+        return equipmentRepository.findByHospitalId(hospitalId);
+        // 
+ 
     }
-
-    
+ 
+   
 }
