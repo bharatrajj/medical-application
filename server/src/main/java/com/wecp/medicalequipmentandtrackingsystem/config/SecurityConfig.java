bd49b81548621a7123e8e7744d7742151c6de4a2
username <wecp-dummy@gmail.com>
2024-02-29 04:07:54 +0000

    New Update

--- server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/config/SecurityConfig.java
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/config/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.wecp.medicalequipmentandtrackingsystem.config;
-
+ 
 import com.wecp.medicalequipmentandtrackingsystem.jwt.JwtRequestFilter;
 import com.wecp.medicalequipmentandtrackingsystem.service.UserService;
-
-import javax.servlet.http.HttpServletRequest;
-
+ 
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -21,85 +21,62 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-
+ 
+ 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
     @Autowired
     private JwtRequestFilter authFilter;
-
+ 
     @Bean
-    // authentication
     public UserDetailsService userDetailsService() {
         return new UserService();
     }
-
+ 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-
-                .authorizeRequests()
-                .antMatchers("/api/user/login").permitAll()
-                .antMatchers("/api/user/register").permitAll()
-                .antMatchers("/api/hospital/create").permitAll()
-                .antMatchers("/api/hospital/equipment").permitAll()
-                .antMatchers("/api/hospital/maintenance/schedule").permitAll()
-                .antMatchers("/api/hospital/order").permitAll()
-                .antMatchers("/api/hospital/equipment").permitAll()
-                .antMatchers("/api/hospital/equipment/**").permitAll()
-                .antMatchers("/api/hospitals").permitAll()
-                .antMatchers("/api/technician/maintenance").permitAll()
-                .antMatchers("/api/technician/maintenance/update/**").permitAll()
-                .antMatchers("/api/supplier/orders").permitAll()
-                .antMatchers("/api/supplier/order/update/**").permitAll()
-                    .antMatchers("/api/**").authenticated()
-                .and()
-                .sessionManagement()
+        return http.cors().and().csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/user/login").permitAll()
+            .antMatchers("/api/user/register").permitAll()
+            .antMatchers(HttpMethod.POST,"/api/hospital/create").hasAuthority("HOSPITAL")
+            .antMatchers(HttpMethod.POST,"/api/hospital/equipment").hasAuthority("HOSPITAL")
+            .antMatchers(HttpMethod.POST,"/api/hospital/maintenance/schedule").hasAuthority("HOSPITAL")
+            .antMatchers(HttpMethod.POST,"/api/hospital/order").hasAuthority("HOSPITAL")
+            .antMatchers(HttpMethod.GET,"/api/hospital/equipment").hasAuthority("HOSPITAL")
+            .antMatchers(HttpMethod.GET,"/api/hospital/equipment/**").hasAuthority("HOSPITAL")
+            .antMatchers(HttpMethod.GET,"/api/hospitals").hasAuthority("HOSPITAL")
+            .antMatchers(HttpMethod.GET,"/api/technician/maintenance").hasAuthority("TECHNICIAN")
+            .antMatchers(HttpMethod.PUT,"/api/technician/maintenance/update/**").hasAuthority("TECHNICIAN")
+            .antMatchers(HttpMethod.GET,"/api/supplier/orders").hasAuthority("SUPPLIER")
+            .antMatchers(HttpMethod.PUT,"/api/supplier/order/update/**").hasAuthority("SUPPLIER")
+                .antMatchers("/api/**").authenticated()
+            .and()
+            .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider())
-                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+            .and()
+            .authenticationProvider(authenticationProvider())
+            .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
     }
-
+ 
     @Bean
     public PasswordEncoder passwordEncoders() {
         return new BCryptPasswordEncoder();
     }
-
+ 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoders());
         return authenticationProvider;
     }
-
+ 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-
-    private static class CustomRequestMatcher implements RequestMatcher {
-        private String[] patterns;
-
-        public CustomRequestMatcher(String... patterns) {
-            this.patterns = patterns;
-        }
-
-        @Override
-        public boolean matches(HttpServletRequest request) {
-            // TODO Auto-generated method stub
-            String requestURI = request.getRequestURI();
-            for (String pattern : patterns) {
-                if (requestURI.matches(pattern)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
 }
